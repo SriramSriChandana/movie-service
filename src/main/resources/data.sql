9bc89693dadad00bd2a5bac0f804d4b965ca375a
SriramSriChandana <srichandanasriram2003@gmail.com>
2025-01-09 15:07:35 +0530

    4th commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO movie_details (movie_title, movie_language, movie_release_date, movie_image_url) 
+VALUES 
+    ('The Shawshank Redemption', 'English', '1994-09-22', ' '),
+    ('The Dark Knight', 'English', '2008-07-18', ' '),
+    ('Inception', 'English', '2010-07-16', ' '),
+    ('Parasite', 'Korean', '2019-05-30', ' ');
